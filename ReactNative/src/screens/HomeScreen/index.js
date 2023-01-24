5a1f7e798a82012b1b0453ba128c3c2b98e2d061
Colin <colin.picolet@telecom-paris.fr>
2023-01-24 10:29:57 +0100

    tests

--- ReactNative/src/screens/HomeScreen/index.js
+++ ReactNative/src/screens/HomeScreen/index.js
@@ -4,10 +4,30 @@ import {Text, View} from "react-native";
 const index = () => {
     return (
         <View>
-            <Text style={{fontSize: 24, alignSelf: 'center',}}>HomeScreen</Text>
+            <Text style={{fontSize: 24, alignSelf: 'right',}}>HomeScreen</Text>
         </View>
         
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    root:{
+        alignItems: 'center',
+        padding: 20,
+    },
+    logo: {
+        width: '70%',
+        maxWidth: 300,
+        maxHeight: 200,
+        marginBottom: 20,
+    },
+    title:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#051C60',
+        margin: 10,
+
+    },
+});
 
 export default index
