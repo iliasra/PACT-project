97eff07b6fed6873b8560bddae7301ba71619ab1
Colin Picolet <colin.picolet@telecom-paris.fr>
2023-03-24 10:46:49 +0100

    on avance bien

--- ReactNative/src/screens/HomeScreen/HomeScreen.js
+++ ReactNative/src/screens/HomeScreen/HomeScreen.js
@@ -4,13 +4,14 @@ import CustomInput from "../../components/CustomInput"
 import CustomButton from "../../components/CustomButton"
 import {useNavigation} from '@react-navigation/native'
 import CustomLogOut from "../../components/CustomLogOut"
+import BoutonSalle from "../../components/BoutonSalle"
 
 const HomeScreen = () => {
     const navigation = useNavigation();
    
-    const onAttemptConnection = () => {
-        console.warn("veuillez attendre")
-    }
+    const Square = () => {
+        return <View style={styles.square} />;
+      };
 
     const logOutButton = () => {
         navigation.navigate("SignIn")
@@ -20,29 +21,29 @@ const HomeScreen = () => {
         <ScrollView>
         <View style={styles.root1}>
             <Text style={styles.megatitle}>Balance ton bip!</Text>
+
+            <View style={styles.squareTopLeft}></View>
+            <View style={styles.squareTopRight}></View>
+            <View style={styles.squareBottomLeftAlert}></View>
+            <View style={styles.squareBottomRight}></View>
+
+
+
+            <View style={styles.rootLogOut}>
             <CustomLogOut 
-            text="bouton 1"
-            onPress={logOutButton}
-            type="PRIMARY"
-            />
-            <View style={styles.root2}>
-            <CustomLogOut 
-                text="bouton 2"
+                text="Log Out"
                 onPress={logOutButton}
-                type="PRIMARY"
+                type="TERTIARY"
             />
+            </View>
+
+        
+        
         </View>
-        </View>
+        
 
         
         </ScrollView>
-        /*<View style={styles.root2}>
-            <CustomLogOut 
-                text="bouton 2"
-                onPress={logOutButton}
-                type="PRIMARY"
-            />
-        </View>*/
     );
 };
 
@@ -51,12 +52,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 20,
     },
-    root2:{
+    rootSalle:{
+        alignItems: 'center',
+        position:"relative"
+    },
+    rootLogOut:{
         alignItems: 'center',
         padding: 20,
         flex:1,
         position:"relative",
-        bottom:
+        marginTop: 100
     },
     logo: {
         width: '70%',
@@ -77,7 +82,94 @@ const styles = StyleSheet.create({
         color: '#051C60',
         margin: 15,
         padding:30,
+        marginTop:30
     },
+    
+
+    //la zone n'a pas d'alerte donc elle est blanche
+    squareTopLeft: {
+        width: 100,
+        height: 100,
+        backgroundColor: "white",
+        marginTop:20,
+        marginLeft:-100,
+        borderColor:"black",
+        borderWidth:4,
+        borderBottomWidth:2,
+        borderRightWidth:2
+        },
+    squareTopRight: {
+        width: 100,
+        height: 100,
+        backgroundColor: "white",
+        marginTop:-100,
+        marginLeft:100,
+        borderColor:"black",
+        borderWidth:4,
+        borderBottomWidth:2
+        },
+    squareBottomLeft: {
+        width: 100,
+        height: 100,
+        backgroundColor: "white",
+        marginTop:0,
+        marginLeft:-100,
+        borderColor:"black",
+        borderWidth:4,
+        borderRightWidth:2
+        },
+    squareBottomRight: {
+        width: 100,
+        height: 100,
+        backgroundColor: "white",
+        marginTop:-100,
+        marginLeft:100,
+        borderColor:"black",
+        borderWidth:4
+        },
+
+
+    //la zone a une alerte donc elle est rouge
+    squareTopLeftAlert: {
+        width: 100,
+        height: 100,
+        backgroundColor: "red",
+        marginTop:20,
+        marginLeft:-100,
+        borderColor:"black",
+        borderWidth:4,
+        borderBottomWidth:2,
+        borderRightWidth:2
+        },
+    squareTopRightAlert: {
+        width: 100,
+        height: 100,
+        backgroundColor: "red",
+        marginTop:-100,
+        marginLeft:100,
+        borderColor:"black",
+        borderWidth:4,
+        borderBottomWidth:2
+        },
+    squareBottomLeftAlert: {
+        width: 100,
+        height: 100,
+        backgroundColor: "red",
+        marginTop:0,
+        marginLeft:-100,
+        borderColor:"black",
+        borderWidth:4,
+        borderRightWidth:2
+        },
+    squareBottomRightAlert: {
+        width: 100,
+        height: 100,
+        backgroundColor: "red",
+        marginTop:-100,
+        marginLeft:100,
+        borderColor:"black",
+        borderWidth:4
+        }
 });
 
 export default HomeScreen
